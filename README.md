fff8594dc160bd182b5f9dd0407a6059999e6d93
Aashutosh Rathi <aashutoshrathi@gmail.com>
2023-03-14 12:24:03 +0530

    :sparkles: feat(v0.0.1): basic working version

---
+++ README.md
@@ -0,0 +1,27 @@
+# Undependable
+
+Dependency management is hard and dependabot makes it easy. But it also floods PR list with a lot of PRs. This extension helps you toggle the visibility of dependabot PRs.
+
+> **Note**
+> This is for very very lazy and my kinda people, who don't want to do anything manually.
+
+<!-- ## Installation -->
+
+<!-- [link-chrome]: https://chrome.google.com/webstore/detail/mongo-ip-updater/cklilnpehpogpeoeklbefjbjafcnlofj 'Version published on Chrome Web Store'
+
+[<img src="https://raw.githubusercontent.com/alrra/browser-logos/90fdf03c/src/chrome/chrome.svg" width="48" alt="Chrome" valign="middle">][link-chrome] [<img valign="middle" src="https://img.shields.io/chrome-web-store/v/cklilnpehpogpeoeklbefjbjafcnlofj.svg?label=%20">][link-chrome] and other Chromium browsers -->
+
+## Development
+
+- Get it locally -
+
+  ```sh
+  $ git clone https://github.com/aashutoshrathi/undependable.git
+  ```
+
+  **OR**
+
+  Download the latest version here: [Undependable ✨](https://github.com/aashutoshrathi/undependable/archive/main.zip)
+
+- Go to `chrome://extensions/` & enable Developer Mode.
+- Click on Load Unpacked Extension and Open/Select the folder.
---
+++ src/background.js
@@ -0,0 +1,44 @@
+const MANIFEST = chrome.runtime.getManifest();
+const onInstalled = async () => {
+  // TODO: Use this on options page
+  chrome.storage.sync.set({ version: `v${MANIFEST.version}` });
+};
+
+// TODO: use this
+const toggleState = () => {
+  chrome.storage.sync.get(['state'], (result) => {
+    const state = result.state || 'off';
+    chrome.storage.sync.set({ state: state === 'on' ? 'off' : 'on' });
+  });
+}
+
+const BASE_QUERY = '?q=is%3Apr+is%3Aopen+';
+const HIDE_DEPENDABOT_QUERY = '-label%3Adependencies+';
+
+const shouldExecuteOnTab = (host, pathname) => {
+  return host === "github.com" && pathname.endsWith("/pulls");
+}
+
+const getAdditionQuery = (search) => {
+  if (!search) {
+    return BASE_QUERY + HIDE_DEPENDABOT_QUERY;
+  }
+  return HIDE_DEPENDABOT_QUERY;
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  const { id, url } = tab;
+  const { search, host, pathname } = new URL(tab.url);
+  if (shouldExecuteOnTab(host, pathname)) {
+    console.log('Running undependabot');
+
+    if (search.includes(HIDE_DEPENDABOT_QUERY)) {
+      chrome.tabs.update(id, { url: url.replace(HIDE_DEPENDABOT_QUERY, '') });
+    } else {
+      chrome.tabs.update(tab.id, { url: tab.url + getAdditionQuery(search) });
+    }
+    // chrome.action.setIcon({ path: '../icons/on-icon.png' });
+  }
+});
+
+chrome.runtime.onInstalled.addListener(onInstalled);
